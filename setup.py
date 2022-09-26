ea8893f38b936b61cfb5f186a369ee07a3e029b1
antoine Lemaire <antolemaire08@gmail.com>
2022-09-26 11:17:54 +0200

    First data visualization notebook

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = [x.strip() for x in content if 'git+' not in x]
 
 setup(name='beat_the_bookies',
       version="1.0",
-      description="Project Description",
+      description="Launching of the project, building the dataset and first models",
       packages=find_packages(),
       install_requires=requirements,
       test_suite='tests',
